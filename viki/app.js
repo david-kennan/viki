242c4460d14d742265d270607de0c76dfb983c16
Ziv Kennan <zivkennan@gmail.com>
2012-08-21 16:05:14 +0800

    cleanup

--- viki/app.js
+++ viki/app.js
@@ -1,3 +1,6 @@
+// This is the node entry point for the viki app
+// 8/12, Sam Greene & Ziv Kennan
+
 // config mongo server
 var mongo_server = 'localhost';
 var mongo_port = '27017';
@@ -25,7 +28,7 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('3489234dkslfjf93ddw90092'));
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
@@ -35,12 +38,15 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/upload', routes.upload);
-app.get('/viewimages', routes.viewimages);
-app.get('/image/get/:imageID', routes.getImage);
-app.post('/upload', routes.uploadImage);
+// route
+app.get('/', routes.index);                       // home page
+app.get('/image/upload', routes.upload);          // upload an image
+app.get('/image/view/all', routes.viewimages);    // view images
+app.get('/image/get/:imageID', routes.getImage);  // get a specific image
+app.post('/image/upload', routes.uploadImage);    // upload an image
 
+// listen
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+  console.log ("Viki is ready...");
 });
