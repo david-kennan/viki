be47d66e79a2e51a039a9f469dbd992c7159553a
sgreene8 <sgreene@uchicago.edu>
2012-08-21 15:41:06 +0800

    Got image fetches working

--- viki/routes/index.js
+++ viki/routes/index.js
@@ -7,8 +7,6 @@ var Image = mongoose.model('Image', require('../models/image.js'));
  */
 
 exports.index = function(req, res){
-//    var testImage = new Image({name:'test1', dateCreated: new Date(), data:'this is data'});
-//    testImage.save();
   res.render('index', { title: 'Express' });
 };
 
@@ -17,57 +15,26 @@ exports.upload = function(req, res) {
 };
 
 exports.uploadImage = function(req, res) {
+    var mongo = require('mongodb'),
+    Server = mongo.Server,
+    Db = mongo.Db,
+    Grid = mongo.Grid,
+    Fs = require('fs');
     
-    var Db = require('mongodb').Db,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
-    assert = require('assert'),
-    fs = require('fs');
+    var server = new Server('localhost', 27017, {auto_reconnect: true});
+    var db = new Db('viki_dev', server);
     
-    var db = new Db('viki_dev', new Server("127.0.0.1", 27017,
-                                                    {auto_reconnect: false, poolSize: 1}), {native_parser: false});
-    
-    // Establish connection to db
     db.open(function(err, db) {
-            // Our file ID
-        var fileId = new ObjectID();
-            
-        // Open a new file
-        var gridStore = new GridStore(db, fileId, 'w');
-            
-        // Read the filesize of file on disk (provide your own)
-        var fileSize = fs.statSync(req.files.filedata.path).size;
-        // Read the buffered data for comparision reasons
-        var data = fs.readFileSync(req.files.filedata.path);
-            
-        // Open the new file
-        gridStore.open(function(err, gridStore) {
-                           
-        // Write the file to gridFS
-            gridStore.writeFile(req.files.filedata.path, function(err, doc) {
-                gridStore.read(fileId, function(err, fileData) {
-                    //console.log('original is ' + fileSize + ', server is ' + fileData.length);
-                    //res.writeHead(200, {'content-type':'image/jpeg'});
-                    //res.end(fileData, 'binary');
-                    res.redirect('/');
-                    gridStore.close();
-                    db.close();
-                    var imagename = req.body.imagename;
-                    var newImg = new Image({name:imagename, dataid:fileId, dateCreated: new Date()});
-                    newImg.save();
-                    
-                });
+        var grid = new Grid(db, 'fs');
+        Fs.readFile(req.files.filedata.path, function(err, data) {
+            grid.put(data, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, fileInfo) {
+                var newImg = new Image({name:req.body.imagename, dataid:fileInfo._id, dateCreated: new Date()});
+                newImg.save();
+                db.close();
+                res.redirect('/');
             });
         });
     });
-//    console.log(req.files.filedata.path);
-    //    console.log(req.files.filedata.type);
-    //newImg.save();
 }
 
 exports.viewimages = function (req, res) {
@@ -79,34 +46,22 @@ exports.viewimages = function (req, res) {
 };
 
 exports.getImage = function (req, res) {
-    var Db = require('mongodb').Db,
-    Server = require('mongodb').Server,
-    ReplSetServers = require('mongodb').ReplSetServers,
-    ObjectID = require('mongodb').ObjectID,
-    Binary = require('mongodb').Binary,
-    GridStore = require('mongodb').GridStore,
-    Code = require('mongodb').Code,
-    BSON = require('mongodb').pure().BSON,
-    assert = require('assert'),
-    fs = require('fs');
+    var mongo = require('mongodb'),
+    Server = mongo.Server,
+    Db = mongo.Db,
+    Grid = mongo.Grid,
+    Fs = require('fs'),
+    ObjectID = mongo.ObjectID;
     
-    var db = new Db('viki_dev', new Server("127.0.0.1", 27017,
-                                           {auto_reconnect: false, poolSize: 1}), {native_parser: false});
+    var server = new Server('localhost', 27017, {auto_reconnect: true});
+    var db = new Db('viki_dev', server);
     
     db.open(function(err, db) {
-            var gridStore = new GridStore(db, req.params.imageID, 'r');
-            gridStore.open(function(err, gridStore) {
-                gridStore.read(req.params.imageID, function (err, fileData) {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        res.writeHead(200, {'Content-Type':'image/jpg'});
-                        res.end(fileData);
-                    }
-                });
-            });
+        var grid = new Grid(db, 'fs');
+        grid.get(ObjectID.createFromHexString(req.params.imageID), function(err, data) {
+            res.writeHead(200, {'content-type':'image/jpeg'});
+            res.end(data, 'binary');
+            db.close();
+        });
     });
-
-    
 };
