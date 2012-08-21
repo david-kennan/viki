76a8de59667aab66e655fbfa56428386f151879f
sgreene8 <sgreene@uchicago.edu>
2012-08-21 16:34:56 +0800

    Got db instance from mongoose in image upload handler

--- viki/routes/index.js
+++ viki/routes/index.js
@@ -1,6 +1,8 @@
-
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var Image = mongoose.model('Image', require('../models/image.js'));
+var Fs = require('fs');
+var Grid = mongo.Grid;
 
 /*
  * GET home page.
@@ -15,24 +17,12 @@ exports.upload = function(req, res) {
 };
 
 exports.uploadImage = function(req, res) {
-    var mongo = require('mongodb'),
-    Server = mongo.Server,
-    Db = mongo.Db,
-    Grid = mongo.Grid,
-    Fs = require('fs');
-    
-    var server = new Server('localhost', 27017, {auto_reconnect: true});
-    var db = new Db('viki_dev', server);
-    
-    db.open(function(err, db) {
-        var grid = new Grid(db, 'fs');
-        Fs.readFile(req.files.filedata.path, function(err, data) {
-            grid.put(data, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, fileInfo) {
-                var newImg = new Image({name:req.body.imagename, dataid:fileInfo._id, dateCreated: new Date()});
-                newImg.save();
-                db.close();
-                res.redirect('/');
-            });
+    var grid = new Grid(mongoose.connection.db);
+    Fs.readFile(req.files.filedata.path, function(err, data) {
+        grid.put(data, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, fileInfo) {
+            var newImg = new Image({name:req.body.imagename, dataid:fileInfo._id, dateCreated: new Date()});
+            newImg.save();
+            res.redirect('/');
         });
     });
 }
@@ -46,11 +36,8 @@ exports.viewimages = function (req, res) {
 };
 
 exports.getImage = function (req, res) {
-    var mongo = require('mongodb'),
-    Server = mongo.Server,
+    var Server = mongo.Server,
     Db = mongo.Db,
-    Grid = mongo.Grid,
-    Fs = require('fs'),
     ObjectID = mongo.ObjectID;
     
     var server = new Server('localhost', 27017, {auto_reconnect: true});
