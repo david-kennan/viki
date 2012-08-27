0d15af3a4534ccb5f8ef81b4604650f076ee4d45
Ziv Kennan <zivkennan@gmail.com>
2012-08-27 19:27:58 +0800

    added message if no images exist in db using promises

--- viki/public/scripts/view_images.js
+++ viki/public/scripts/view_images.js
@@ -1,34 +1,47 @@
 debug.log('loaded view_images.js');
+
 require(["dojo/store/JsonRest", "dojo/dom", "dojo/dom-geometry", "dijit/registry"], function(JsonRest, dom, domGeom, registry) {
-    function numVisibleImages() {
-        var dims = domGeom.getContentBox(dom.byId("welcome"));
-        var imagesWide = Math.floor(dims.w / 106);
-        var imagesHigh = Math.floor(dims.h / 106);
-        return {number: imagesWide * imagesHigh, imageDim: dims.w / imagesWide};
+  // todo wrap this all in a scrollable view inheriting class
+  function numVisibleImages() {
+    var dims = domGeom.getContentBox(dom.byId("welcome"));
+    var imagesWide = Math.floor(dims.w / 106);
+    var imagesHigh = Math.floor(dims.h / 106);
+    return {number: imagesWide * imagesHigh, imageDim: dims.w / imagesWide};
+  }
+  var imgDispInfo = numVisibleImages();
+  var numImages = imgDispInfo.number * 2;  // get two pages at a time
+  var pagesDisplayed = 0;
+  var imageDiv = dom.byId('imageCollection');
+  var imageWidth = imgDispInfo.imageDim - 6;
+
+  // To do: show message if there are no images
+
+  var imageStore = new JsonRest({target:'/image/view/all'});
+  function getPage() {
+    var images = imageStore.query({type:'JSON', topic:'Outdoors', pageSize: numImages, pagesViewed:pagesDisplayed}).map(function(image) {
+      var imageHTML = "<a href='/image/get/" + image.dataid + "'><img src='/image/get/" + image.thumbnailid 
+      + "' height=" + imageWidth + "width=" + imageWidth + "></img></a>";
+    imageDiv.innerHTML += imageHTML;
+    return 0;
+    });
+    pagesDisplayed++;
+    return images;
+  }
+
+  // get the first set of images, showing a message if there are no images
+  getPage().then(function(d){
+    if (!d.length) {
+      imageDiv.innerHTML = "There are no images in " + appName + ", use the upload tab to add images.";
     }
-    var imgDispInfo = numVisibleImages();
-    var numImages = imgDispInfo.number * 2;
-    var pagesDisplayed = 0;
-    var imageDiv = dom.byId('imageCollection');
-    var imageWidth = imgDispInfo.imageDim - 6;
-    // To do: show message if there are no images
-    var imageStore = new JsonRest({target:'/image/view/all'});
-    function getPage() {
-        var images = imageStore.query({type:'JSON', topic:'Outdoors', pageSize: numImages, pagesViewed:pagesDisplayed});
-        images.map(function(image) {
-                   var imageHTML = "<a href='/image/get/" + image.dataid + "'><img src='/image/get/" + image.thumbnailid + "' height=" + imageWidth + "width=" + imageWidth + "></img></a>";
-                   imageDiv.innerHTML += imageHTML;
-        });
-        pagesDisplayed++;
+  });
+
+  // override the scroll adjust function to watch for scroll events
+  registry.byId("viewimages").adjustDestination = function (to, pos) {
+    var dim = this.getDim();
+    var ch = dim.c.h;
+    var dh = dim.d.h;
+    if (to.y < dh - ch) {
+      getPage();
     }
-    getPage();
-    registry.byId("viewimages").adjustDestination = function (to, pos) {
-        var dim = this.getDim();
-        var ch = dim.c.h;
-        var dh = dim.d.h;
-        if (to.y < dh - ch) {
-        console.log('reached end');
-        getPage();
-        }
-    };
+  };
 });
--- viki/routes/index.js
+++ viki/routes/index.js
@@ -12,96 +12,98 @@ exports.index = function(req, res){
 };
 
 exports.upload = function(req, res) {
-    res.render('upload');
+  res.render('upload');
 };
 
 exports.uploadImage = function(req, res) {
   if (req.files.filedata.length != 0) {
     // Image processing
     var path = req.files.filedata.path;
-      console.log(path);
+    console.log(path);
     var util = require('util');
     var exec = require('child_process').exec;
     exec('file -b --mime-type ' + path, function(error, stdout, stderr) {
-        if (!(stdout == 'image/jpeg\n' || stdout == 'image/png\n' || stdout == 'image/tiff\n' || stdout == 'image/tif\n' || stdout == 'image/x-tif\n' || stdout == 'image/x-tiff\n' || stdout == 'image/bmp\n' || stdout == 'image/x-bmp\n' || stdout == 'image/x-bitmap\n' || stdout == 'image/x-xbitmap\n' || stdout == 'image/x-win-bitmap\n' || stdout == 'image/x-windows-bitmap\n' || stdout == 'image/ms-bmp\n' || stdout == 'image/x-ms-bmp\n')) {
-            res.writeHead(200);
-            res.end("Invalid file type uploaded: please upload a JPEG, PNG, TIFF, or BMP image");
-            return;
+      if (!(stdout == 'image/jpeg\n' || stdout == 'image/png\n' || stdout == 'image/tiff\n' || stdout == 'image/tif\n' || stdout == 'image/x-tif\n' || stdout == 'image/x-tiff\n' || stdout == 'image/bmp\n' || stdout == 'image/x-bmp\n' || stdout == 'image/x-bitmap\n' || stdout == 'image/x-xbitmap\n' || stdout == 'image/x-win-bitmap\n' || stdout == 'image/x-windows-bitmap\n' || stdout == 'image/ms-bmp\n' || stdout == 'image/x-ms-bmp\n')) {
+        res.writeHead(200);
+        res.end("Invalid file type uploaded: please upload a JPEG, PNG, TIFF, or BMP image");
+        return;
+      }
+      var gm = require('gm');
+      var original = gm(path);
+      original.setFormat('jpeg');
+      original.interlace('Line');
+      original.size(function(err, value) {
+        var exceedWidth = value.width > 800;
+        var exceedHeight = value.height > 600;
+        if (exceedWidth || exceedHeight) {
+          var widthFactor = value.width / 800;
+          var heightFactor = value.height / 600;
+          var overallFactor = widthFactor > heightFactor ? widthFactor : heightFactor;
+          original.resize(value.width/overallFactor, value.height/overallFactor);
         }
-        var gm = require('gm');
-        var original = gm(path);
-        original.setFormat('jpeg');
-        original.interlace('Line');
-        original.size(function(err, value) {
-            var exceedWidth = value.width > 800;
-            var exceedHeight = value.height > 600;
-            if (exceedWidth || exceedHeight) {
-                var widthFactor = value.width / 800;
-                var heightFactor = value.height / 600;
-                var overallFactor = widthFactor > heightFactor ? widthFactor : heightFactor;
-                original.resize(value.width/overallFactor, value.height/overallFactor);
-            }
-            original.stream(function(err, stdout, stderr) {
-                var dataArray = [];
-                stdout.on('data', function (data) {
-                    dataArray.push(data);
-                });
-                stdout.on('close', function() {
-                    require('bufferjs');
-                    var fullImage = Buffer.concat(dataArray);
-                    var grid = new Grid(mongoose.connection.db);
-                    grid.put(fullImage, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, origInfo) {
-                        original.size(function(err, value) {
-                            var landscape = value.width > value.height;
-                            var maxDimension = landscape ? value.width : value.height;
-                            var minDimension = landscape ? value.height : value.width;
-                            if (maxDimension < 100) {
-                                var newImg = new Image({name:req.body.imagename, dataid:origInfo._id, thumbnailid: null, dateCreated: new Date(), topic: 'Outdoors'});
-                                newImg.save();
-                                res.writeHead(200);
-                                return;
-                            }
-                            var resizeFactor = minDimension/thumbnailSize;
-                            original.resize(value.width/resizeFactor, value.height/resizeFactor);
-                            var cropX = landscape ? (value.width/resizeFactor - thumbnailSize) / 2 : 0;
-                            var cropY = landscape ? 0 : (value.height/resizeFactor - thumbnailSize) / 2;
-                            original.crop(thumbnailSize, thumbnailSize, cropX, cropY);
-                            original.stream(function(err, stdout, stderr) {
-                                var thumbArray = [];
-                                stdout.on('data', function(data) {
-                                    thumbArray.push(data);
-                                });
-                                stdout.on('close', function() {
-                                    var thumbImage = Buffer.concat(thumbArray);
-                                    grid.put(thumbImage, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, thumbInfo) {
-                                        if (err) console.log(err);
-                                        Topic.find({name: req.body.imagetopic}, function(err, topics) {
-                                            var imageTopic;
-                                            if (topics.length > 1) {
-                                                res.end(200, 'Error: there is more than one topic with name ' + req.body.imagetopic);
-                                                return;
-                                            }
-                                            else if (topics.length == 0) {
-                                                imageTopic = new Topic({name:req.body.imagetopic, description: 'A description of this topic'});
-                                                imageTopic.save();
-                                            }
-                                            else {
-                                                imageTopic = topics[0];
-                                            }
-                                            var newImg = new Image({name:req.body.imagename, dataid:origInfo._id, thumbnailid: thumbInfo._id, dateCreated: new Date(), topicid: imageTopic._id});
-                                            newImg.save();
-                                            res.writeHead(200);
-                                        });
-                                    });
-                                });
-                            });
-                        });
+        original.stream(function(err, stdout, stderr) {
+          var dataArray = [];
+          stdout.on('data', function (data) {
+            dataArray.push(data);
+          });
+          stdout.on('close', function() {
+            require('bufferjs');
+            var fullImage = Buffer.concat(dataArray);
+            var grid = new Grid(mongoose.connection.db);
+            grid.put(fullImage, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, origInfo) {
+              original.size(function(err, value) {
+                var landscape = value.width > value.height;
+                var maxDimension = landscape ? value.width : value.height;
+                var minDimension = landscape ? value.height : value.width;
+                if (maxDimension < 100) {
+                  var newImg = new Image({name:req.body.imagename, dataid:origInfo._id, thumbnailid: null, dateCreated: new Date(), topic: 'Outdoors'});
+                  newImg.save();
+                  res.writeHead(200);
+                  res.end();
+                  return;
+                }
+                var resizeFactor = minDimension/thumbnailSize;
+                original.resize(value.width/resizeFactor, value.height/resizeFactor);
+                var cropX = landscape ? (value.width/resizeFactor - thumbnailSize) / 2 : 0;
+                var cropY = landscape ? 0 : (value.height/resizeFactor - thumbnailSize) / 2;
+                original.crop(thumbnailSize, thumbnailSize, cropX, cropY);
+                original.stream(function(err, stdout, stderr) {
+                  var thumbArray = [];
+                  stdout.on('data', function(data) {
+                    thumbArray.push(data);
+                  });
+                  stdout.on('close', function() {
+                    var thumbImage = Buffer.concat(thumbArray);
+                    grid.put(thumbImage, {metadata:{category:'image'}, content_type: 'image/jpeg'}, function(err, thumbInfo) {
+                      if (err) console.log(err);
+                      Topic.find({name: req.body.imagetopic}, function(err, topics) {
+                        var imageTopic;
+                        if (topics.length > 1) {
+                          res.end(200, 'Error: there is more than one topic with name ' + req.body.imagetopic);
+                          return;
+                        }
+                        else if (topics.length == 0) {
+                          imageTopic = new Topic({name:req.body.imagetopic, description: 'A description of this topic'});
+                          imageTopic.save();
+                        }
+                        else {
+                          imageTopic = topics[0];
+                        }
+                      var newImg = new Image({name:req.body.imagename, dataid:origInfo._id, thumbnailid: thumbInfo._id, dateCreated: new Date(), topicid: imageTopic._id});
+                      newImg.save();
+                      res.writeHead(200);
+                      res.end();
+                      });
                     });
+                  });
                 });
+              });
             });
+          });
         });
+      });
     });
-     
+
   }
   else {
     res.writeHead(200);
@@ -110,11 +112,11 @@ exports.uploadImage = function(req, res) {
 }
 
 exports.viewimages = function (req, res) {
-    var query = req.query;
+  var query = req.query;
   if (query.type == "JSON") {
     if (query.topic) {
-        Topic.find({name: query.topic}, function (err, topics) {
-                   // for developement only //
+      Topic.find({name: query.topic}, function (err, topics) {
+        // for developement only //
         if (topics.length == 0) {
           //var tmptopic = new Topic({name: 'Outdoors', description: 'Outdoor Images', category: 'Nature'});
           //tmptopic.save();
@@ -144,14 +146,14 @@ exports.viewimages = function (req, res) {
 };
 
 exports.getImage = function (req, res) {
-    var ObjectID = mongo.ObjectID;
-    
-    var grid = new Grid(mongoose.connection.db);
-    grid.get(ObjectID.createFromHexString(req.params.imageID), function(err, data) {
-        if (err) {
-          console.log(err);
-        }
-        res.writeHead(200, {'content-type':'image/jpeg'});
-        res.end(data, 'binary');
-    });
+  var ObjectID = mongo.ObjectID;
+
+  var grid = new Grid(mongoose.connection.db);
+  grid.get(ObjectID.createFromHexString(req.params.imageID), function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+    res.writeHead(200, {'content-type':'image/jpeg'});
+    res.end(data, 'binary');
+  });
 };
