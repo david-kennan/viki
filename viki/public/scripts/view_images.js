d2ed932eaeb359606ffcfb2e8ba210152fc85324
sgreene8 <sgreene@uchicago.edu>
2012-08-28 14:02:20 +0800

    Working on the single image view

--- viki/public/scripts/view_images.js
+++ viki/public/scripts/view_images.js
@@ -1,6 +1,6 @@
 debug.log('loaded view_images.js');
 
-require(["dojo/store/JsonRest", "dojo/dom", "dojo/dom-geometry", "dijit/registry", "dojo/dom-construct"], function(JsonRest, dom, domGeom, registry, domConstruct) {
+require(["dojo/store/JsonRest", "dojo/dom", "dojo/dom-geometry", "dijit/registry", "dojo/dom-construct", "dojo/dom-style", "dojo/on", "dojo/_base/fx"], function(JsonRest, dom, domGeom, registry, domConstruct, domStyle, on, baseFx, dojoHTML) {
   // todo wrap this all in a scrollable view inheriting class
   function numVisibleImages() {
     var dims = domGeom.getContentBox(dom.byId("welcome"));
@@ -19,8 +19,26 @@ require(["dojo/store/JsonRest", "dojo/dom", "dojo/dom-geometry", "dijit/registry
   var imageStore = new JsonRest({target:'/image/view/all'});
   function getPage() {
     var images = imageStore.query({type:'JSON', topic:'Outdoors', pageSize: numImages, pagesViewed:pagesDisplayed}).map(function(image) {
-    var aElem = domConstruct.create("a", {href: '/image/get/' + image.dataid}, imageDiv);
+    var aElem = domConstruct.create("a", {cursor:'pointer'}, imageDiv);
     domConstruct.create("img", {src: '/image/get/' + image.thumbnailid, height: imageWidth, width: imageWidth}, aElem);
+    
+    on(aElem, "click", function() {
+        console.log('click handler called');
+        var container = dom.byId('imageContainer');
+        var imageElem = domConstruct.create("img", {src: '/image/get/' + image.dataid}, container);
+        on(imageElem, "load", function() { // binding to the load event may not work in all browsers, it is not in the W3C specs
+            var imgSize = domGeom.getContentBox(imageElem);
+            console.log(imgSize);
+            var overlay = dom.byId('imageOverlay');
+            domStyle.set(overlay, "display", "block");
+            baseFx.animateProperty({
+                node: overlay,
+                properties: {opacity: {start:0, end: 100}},
+                duration: 6500,
+            }).play();
+        });
+        
+    });
     return 0;
     });
     pagesDisplayed++;
