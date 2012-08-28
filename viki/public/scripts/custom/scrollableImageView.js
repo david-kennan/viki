6a83e91ff87fd06e57a051c53212ce97d56f633b
sgreene8 <sgreene@uchicago.edu>
2012-08-28 18:21:27 +0800

    Added a close button toto the single image view and sized the overlay properly in iOS

--- viki/public/scripts/custom/scrollableImageView.js
+++ viki/public/scripts/custom/scrollableImageView.js
@@ -1,10 +1,51 @@
 define(["dojo/_base/declare", "dojo/store/JsonRest", "dojo/dom", "dojo/dom-geometry", "dijit/registry", "dojox/mobile/ScrollableView", 
-    "dojox/mobile/ContentPane", "dojo/dom-construct"], 
-    function(declare, JsonRest, dom, domGeom, registry, ScrollableView, ContentPane, domConstruct) {
+    "dojox/mobile/ContentPane", "dojo/dom-construct", "dojo/dom-style", "dojo/_base/fx", "dojo/json", "dojo/on", "dojo/window"], 
+    function(declare, JsonRest, dom, domGeom, registry, ScrollableView, ContentPane, domConstruct, domStyle, baseFx, JSON, on, win) {
       return declare("viki.scrollableImageView", [ScrollableView], {
         // this is now the new widget context
-        clickHandler: function(e) {
-          debug.log("image clicked: " + e);
+        clickHandler: function(image) {
+          debug.log("image clicked: " + image);
+            //var container = dom.byId('imageContainer');
+            var thumbnailView = dom.byId(image);
+            var overlay = dom.byId('imageOverlay');
+            domStyle.set(overlay, "height", win.getBox().h + "px"); // this is needed to properly size the view in iOS
+            var thumbnailBox = domGeom.getMarginBox(thumbnailView);
+            var imageElem = domConstruct.create("img", {src: '/image/get/' + image}, overlay);
+            on(imageElem, "load", function() { // binding to the load event may not work in all browsers, it is not in the W3C specs
+                domStyle.set(overlay, "display", "block");
+                var fullImgDim = domGeom.getMarginBox(imageElem);
+                var divSize = domGeom.getMarginBox(overlay);
+                var imgRatio = fullImgDim.w / fullImgDim.h;
+                var dispRatio = divSize.w / divSize.h;
+                var animProps = {};
+                domStyle.set(imageElem, "left", (thumbnailBox.l + 50) + "px");
+                domStyle.set(imageElem, "top", (thumbnailBox.t + 50 + 44) + "px");
+                // this if/else statement sets properties that center the image
+                if (imgRatio > dispRatio) {
+                    animProps.width = divSize.w;
+                    animProps.left = 0;
+                    animProps.top = (divSize.h - divSize.w / imgRatio) / 2;
+                    domStyle.set(imageElem, "height", "auto");
+                    domStyle.set(imageElem, "width", 0);
+                }
+                else {
+                    animProps.height = divSize.h;
+                    animProps.top = 0;
+                    animProps.left = (divSize.w - divSize.h * imgRatio) / 2;
+                    domStyle.set(imageElem, "width", "auto");
+                    domStyle.set(imageElem, "height", 0);
+                }
+                baseFx.animateProperty({
+                    node: overlay,
+                    properties: {opacity: 1},
+                    duration: 500
+                }).play();
+                baseFx.animateProperty({
+                    node: imageElem,
+                    properties: animProps,
+                    duration: 500
+                }).play();
+            });
         },
         startup: function() {
           debug.log("viki.scrollableImageView init...");
--- viki/public/scripts/view_images.js
+++ viki/public/scripts/view_images.js
@@ -1,6 +1,6 @@
 debug.log('loaded view_images.js');
 
-require(["dojo/dom", "dojo/dom-geometry", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/fx"], function(dom, domGeom, domStyle, domConstruct, baseFx) {
+/*require(["dojo/dom", "dojo/dom-geometry", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/fx"], function(dom, domGeom, domStyle, domConstruct, baseFx) {
     var view = registry.byId('viewimages');
     view.clickhandler = function (image) {
         console.log(image);
@@ -10,7 +10,7 @@ require(["dojo/dom", "dojo/dom-geometry", "dojo/dom-style", "dojo/dom-construct"
         var thumbnailStyle = domStyle.getComputedStyle(thumbnailView);
         var thumbnailBox = domGeom.getContentBox(thumbnailView, thumbnailStyle);
         console.log("Thumbnail contentBox is: " + thumbnailBox);
-        var imageElem = domConstruct.create("img", {src: '/image/get/' + image /*, width: "1px", height: "1px", left: thumbnailBox.l + 50, top: thumbnailBox.t + 50*/}, overlay);
+        var imageElem = domConstruct.create("img", {src: '/image/get/' + image /*, width: "1px", height: "1px", left: thumbnailBox.l + 50, top: thumbnailBox.t + 50*//*}, overlay);
         on(imageElem, "load", function() { // binding to the load event may not work in all browsers, it is not in the W3C specs
             var fullImgStyle = domStyle.getComputedStyle(imageElem);
             var fullImgDim = domGeom.getContentBox(imageElem);
@@ -27,4 +27,4 @@ require(["dojo/dom", "dojo/dom-geometry", "dojo/dom-style", "dojo/dom-construct"
             });
         });
     };
-});
+});*/
