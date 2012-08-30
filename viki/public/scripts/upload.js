da50ee82f58b4a407a309862bf58773ee0404717
Ziv Kennan <zivkennan@gmail.com>
2012-08-30 08:54:43 +0000

    made uploader behave more consistently, and provide user feedback on upload

--- viki/public/scripts/upload.js
+++ viki/public/scripts/upload.js
@@ -3,9 +3,14 @@ require(["dojox/mobile/parser", "dojo/dom", "dijit/registry", "dojo/dom-construc
     function(parser, dom, registry, construct, ready) {
       debug.log("loaded upload.js...");
       var uploader;
+      var submitString = "Add Image to Viki";
+
+      // once a file has been uploaded, reset the uploader to it can be used again
       var resetUploader = function () {
-        uploader = newUploader(); 
+        uploader = newUploader();
       }
+      
+      // returns a fresh loader
       var newUploader = function () {
         tmp = new qq.FileUploader({
           uploadButtonText: 'Select Image',
@@ -14,7 +19,12 @@ require(["dojox/mobile/parser", "dojo/dom", "dijit/registry", "dojo/dom-construc
           action: '/image/upload',
           onComplete: function() { 
             construct.destroy(dom.byId("placeholder"));
+            registry.byId('submit').set('value', 'File Uploaded Successfully');
             resetUploader();
+          },
+          onSubmit: function () {
+            registry.byId('submit').set('disabled', false);
+            registry.byId('submit').set('value', submitString);
           },
           allowedExtensions: ['jpg','jpeg','png','bmp','tiff'],
           acceptFiles: ['image/*'],
@@ -22,13 +32,19 @@ require(["dojox/mobile/parser", "dojo/dom", "dijit/registry", "dojo/dom-construc
           autoUpload: false
         });
         dom.byId('gist').value = 'My Cool Image';
+        registry.byId('submit').set('disabled', true);
         return tmp; 
       }
+      
+      // this sets everything up on dom load
       ready(function() {
         uploader = newUploader();
-        registry.byId("submit").on("click", function() {
-          uploader.setParams({imagename: dom.byId('gist').value, imagetopic: dom.byId('topic').value});
-          uploader.uploadStoredFiles();
+        registry.byId('submit').on('click', function() {
+          if (true) {
+            uploader.setParams({imagename: dom.byId('gist').value, imagetopic: dom.byId('topic').value});
+            uploader.uploadStoredFiles();
+          }
         });
       });
+
     });
